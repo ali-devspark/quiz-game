9cf237ef5279b92870178bd15786d684eb4902fd
ali-devspark <ali.devspark@gmail.com>
2026-02-11 16:46:44 +0300

    Try to connect project to cloud db - Neon

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,3 +1,4 @@
+// import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
@@ -6,5 +7,6 @@ export default defineConfig({
     dialect: "sqlite",
     dbCredentials: {
         url: process.env.DATABASE_URL!,
+        token: process.env.DATABASE_AUTH_TOKEN,
     },
 });
